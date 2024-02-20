640174d7241d2335b37e457c78b17a0a8593cb99
vinay961 <vinay100rai@gmail.com>
2024-02-20 21:56:10 +0530

    Hof

---
+++ basicJS03/hof.js
@@ -0,0 +1,19 @@
+// Higher order function--> If we pass a function as argument in another function then it is called HOF.
+// function hof(fun,a,b){
+//     return add(a,b);
+// }
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(hof(add,6,6))
+
+// function greet(){
+//     return fun(){
+//         console.log("I am Vinay");
+//     }
+// }
+// let ans = greet()
+
+// Using higher order function we do async process
